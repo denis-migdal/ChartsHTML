c160d6cae71c75e2143a2b1853187560a02d5632
denmigda <denis.migdal@uca.fr>
2024-06-10 12:41:12 +0200

    Add Bars

--- components/curves/Bars.ts
+++ components/curves/Bars.ts
@@ -15,34 +15,55 @@ export default class Bars extends Dataset {
 
 		const data = super._contentParser(content);
 
-        console.warn("?");
-        return [0.1,0.2,0.3,0.4];
-
 		if(data === undefined)
 			return [];
 
-		return []; //TODO: data.map( (p: [number, number]) => {return {x:p[0],y: p[1]} });
+		return data.map( (p: [number, number]) => {return {x:p[0],y: p[1]} });
+    }
+
+    override _before_chart_update(): void {
+        console.warn(this.chart._chartJS.data.datasets);
     }
 
     override _update() {
         super._update();
 
-        this.dataset.borderWidth   = 1; // 0
-        this.dataset.barPercentage = 1;
-        this.dataset.categoryPercentage = 1;
-        this.dataset.backgroundColor = "green";
-        this.dataset.grouped = false;
+        // h4ck to set min/max values.
+        if(this.dataset.data.length > 1) {
 
+            const data = this.dataset.data;
+            const min = data[0].x;
+            const max = data[data.length-1].x;
 
-        this.dataset.labels = ["1", "2", "3", "4"];
-
-        this.dataset.scales = {
-            x: {
-                labels : [0.5, "2", "3", "4"]
+            let width = Number.POSITIVE_INFINITY;
+            for(let i = 1; i < data.length; ++i) {
+                const w = data[i].x - data[i-1].x;
+                if( w < width)
+                    width = w;
             }
+
+            this.dataset.data = [
+                {x: min -width/2, y:null},
+                ...data,
+                {x: max +width/2, y:null}
+            ]
         }
 
-        //(this.chart._chartJS.options!.scales!.x! as any).labels = [0.5, "2", "3", "4"]; // required for bars (strange...)
+        this.dataset.borderWidth   = 0;
+        this.dataset.barPercentage = 1;
+        this.dataset.categoryPercentage = 2;
+        this.dataset.backgroundColor = "green";
+        this.dataset.inflateAmount = 1; // hide artifacts.
+        this.dataset.grouped = false;
+
+        //this.dataset.barThickness = "flex"; // not working properly...
+
+
+        this.dataset.parsing = false;
+        this.dataset.normalized = true;
+
+
+        //this.dataset.labels = ["1", "2", "3", "4"];
     }
 }
 
