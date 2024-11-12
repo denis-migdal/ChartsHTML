49df86b967825390cbc6e02142dfdbc3af0440ae
denmigda <denis.migdal@uca.fr>
2024-11-12 19:20:32 +0100

    Initial commit

---
+++ dist/dev/index.js
@@ -0,0 +1,8 @@
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.warn('ok');
+
+
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7OztBQUFBQSxRQUFRQyxJQUFJLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9bUFJPSkVDVF9OQU1FXS8uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLndhcm4oJ29rJyk7Il0sIm5hbWVzIjpbImNvbnNvbGUiLCJ3YXJuIl0sInNvdXJjZVJvb3QiOiIifQ==
